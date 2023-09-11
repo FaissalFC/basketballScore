e323bb2c38b7b06521da22491655cd6325cb723e
FaissalFC <142908515+FaissalFC@users.noreply.github.com>
2023-09-11 16:10:06 -0300

    Add files via upload

---
+++ Bskscore/index.js
@@ -0,0 +1,42 @@
+let home = document.getElementById("home")
+let guest = document.getElementById("guest")
+let a = 0
+let b = 0
+
+function addhome1() {
+    a += + 1
+    home.innerText = a
+}
+
+function addhome2() {
+    a += + 2
+    home.innerText = a
+}
+
+function addhome3() {
+    a += + 3
+    home.innerText = a
+}
+
+function addguest1() {
+    b += + 1
+    guest.innerText = b
+}
+
+function addguest2() {
+    b += + 2
+    guest.innerText = b
+}
+
+function addguest3() {
+    b += + 3
+    guest.innerText = b
+}
+
+function eu123() {
+    a = 0
+    b = 0
+    home.innerText = a
+    guest.innerText = b
+}
+ 
